781e117af1902bab46448160a2b52b9bacac91cd
Kysil Ivan <kysilivan@gmail.com>
2016-12-05 17:19:50 +0200

    added flyway config for DB script management; added initial mybatis config file; added Person mapper;

---
+++ src/main/java/my/mybatis/Launcher.java
@@ -0,0 +1,29 @@
+package my.mybatis;
+
+
+import my.mybatis.entity.Person;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Launcher {
+
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            Person person = session.selectOne("mybatis.mapping.PersonMapper.selectPerson", 1);
+            System.out.println("Person:" + person);
+        } finally {
+            session.close();
+        }
+
+    }
+
+}
